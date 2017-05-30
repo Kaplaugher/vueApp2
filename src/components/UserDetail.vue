be02842d0c541baf0d09421a35c07871aeefe516
Kaplaugher <kaplaugher94@gmail.com>
2017-05-30 11:49:17 -0400

    Initial commit

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+      props: {
+        myName: {
+          type: String
+        }
+      },
+      methods: {
+        switchName() {
+          return this.myName.split("").reverse().join("");
+        }
+      }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
